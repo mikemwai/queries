aa792f194c6636835d98baa555b6775835b7de93
mikemwai <mwai.michael@strathmore.edu>
2026-06-01 17:46:19 +0300

    feat: add password reset

--- sql/alter/system_queries.sql
+++ sql/alter/system_queries.sql
@@ -1,1 +1,6 @@
 alter database open; --- Open the database to allow access to system views
+
+alter user account_name
+   account unlock; --- Unlock a specific user account_name
+
+alter user account_name identified by "new_password"; --- Reset the password for the specific user account
