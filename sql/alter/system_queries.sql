597bbb26091a23555d0bef9aefc4521c6de729ec
Michael Mwai <mwai.michael@strathmore.edu>
2026-06-26 13:40:31 +0300

    Add command to change current container to root

--- sql/alter/system_queries.sql
+++ sql/alter/system_queries.sql
@@ -8,3 +8,5 @@ alter user account_name identified by "new_password"; --- Reset the password for
 alter system set sga_target = 600M scope = memory; --- Change the SGA target size in memory (To work with spfile, use scope = spfile)
 
 alter session set nls_date_format = 'YYYY-MM-DD HH24:MI:SS'; --- Change the NLS date format for the current session
+
+alter session set container=cdb$root; ------ Change current container to root
